dd7e682197e1f94374410b34c8d0f24781820f14
YoungJinAn <paul26375@gmail.com>
2018-11-07 23:34:26 +0900

    SW Expert Academy 3307. 최장 증가 부분 수열(쉬움)

--- CodingTest/Solved/SW_Expert_Academy_3307.cpp
+++ CodingTest/Solved/SW_Expert_Academy_3307.cpp
@@ -6,12 +6,13 @@ int mlength;
 
 int main(void)
 {
-	FILE* file = fopen("C:\\Users\\paul5\\Downloads\\sample_input.txt", "r");
+	FILE* file = fopen("sample_input.txt", "r");
 
 	int numTestCase = 0;
 	fscanf(file, "%d", &numTestCase);
-	arr = new int[100000];
-	int* path = new int[100000];
+	arr 		= new int[100000];
+	int* path 	= new int[100000];
+
 	for (int testCount = 1; testCount <= numTestCase; testCount++) {
 		memset(arr, 0, sizeof(int) * 100000);
 
@@ -24,24 +25,16 @@ int main(void)
 		for (int i = 0; i < mlength; i++)
 			fscanf(file, "%d", &arr[i]);
 
-		for (int i = 0; i < mlength-1; i++) {
-			for (int j = i+1; j < mlength; j++) {
+		for (int i = 0; i < mlength-1; i++)
+			for (int j = i+1; j < mlength; j++)
 				if (arr[i] < arr[j] && path[i] == path[j])
 					path[j]++;
-			}
-		}
+
 		int max = 0;
 		for (int i = 0; i < mlength; i++)
 			if (path[i] > max)
 				max = path[i];
+
 		printf("#%d %d\n", testCount, max);
-
-		/*for (int i = 0; i < mlength; i++)
-			printf("%d ", arr[i]);
-		printf("\n");
-
-		for (int i = 0; i < mlength; i++)
-			printf("%d ", path[i]);
-		printf("\n");*/
 	}
 }
