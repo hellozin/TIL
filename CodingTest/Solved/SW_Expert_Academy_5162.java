a3722f9aa25d998975acf621182f34af818629ed
hellozin <paul26375@gmail.com>
2019-03-11 21:16:29 +0900

    5162. 두가지 빵의 딜레마

--- CodingTest/Solved/SW_Expert_Academy_5162.java
+++ CodingTest/Solved/SW_Expert_Academy_5162.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class SW_Expert_Academy_5162 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int numOfTestCase = scanner.nextInt();
-        IntStream.range(0, numOfTestCase).forEach(testCase -> {
+        for (int testCase = 1; testCase <= numOfTestCase; testCase++) {
             int A = scanner.nextInt();
             int B = scanner.nextInt();
             int C = scanner.nextInt();
-            System.out.printf("#%d %d\n", testCase + 1, C / (A < B ? A : B));
-        });
+            System.out.printf("#%d %d\n", testCase, C/(A < B ? A : B));
+        }
         scanner.close();
     }
 }
